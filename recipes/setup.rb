c2ae523334ae8ab87f929dfc7868f1f6a1640f5e
Danielle Fuller <daniellefuller03@yahoo.com>
2018-01-04 18:41:43 +0000

    First commit for workstation cookbook

---
+++ recipes/setup.rb
@@ -0,0 +1,17 @@
+package 'tree' do
+  action :install
+end
+
+package 'vim-enhanced'
+
+package 'ntp'
+
+package 'git' do
+  action :install
+end
+
+file 'etc/motd' do
+  content 'This server is the property of Danielle'
+  action :create
+end
+
